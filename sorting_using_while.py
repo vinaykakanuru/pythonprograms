3e878619f6a5f9b5a352cd1e1b3995f7c1e37084
K VINAY KUMAR REDDY <70269419+kakanuruvinay@users.noreply.github.com>
2021-01-17 22:18:38 +0530

    made changes in argument name

--- sorting_using_while.py
+++ sorting_using_while.py
@@ -3,10 +3,10 @@ l = [64, 34, 25, 12, 22, 11, 90]
 # iterating over given list and popping up the min element and storing in result varibale
 
 
-def sort_using_while(list):
+def sort_using_while(given_list):
     res = []
-    while l:
-        res.append(l.pop(l.index(min(l))))
+    while given_list:
+        res.append(given_list.pop(given_list.index(min(given_list))))
     return res
 
 
