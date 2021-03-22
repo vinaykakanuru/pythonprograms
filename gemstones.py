f3d60f11db3af81624e1b9e2477b335694553130
K VINAY KUMAR REDDY <kvinaykumarreddy1995@gmail.com>
2021-03-22 23:31:26 +0530

    method wise segregation

--- gemstones.py
+++ gemstones.py
@@ -3,20 +3,27 @@
 # input
 arr = ['abcde', 'bcde', 'cfgh']
 
-# output should be 'c' because 'c' is the alphabet present in all elements in the given array
+# output should be 'c' because 'c' is the common alphabet present in all elements in the given array
+
+# method 1 using set
 
 
-def gemstones(arr):
-    # method 1
+def gemstones_method_1(arr):
     arr1 = [set(i) for i in arr]
     return set.intersection(*arr1)
 
-    # method 2 using ascii_lowercase
-    # from string import ascii_lowercase
-    # all_chars = set(ascii_lowercase)
-    # for i in arr:
-    #     all_chars = all_chars.intersection(set(i))
-    # return all_chars
+
+print(gemstones_method_1(arr))
 
 
-print(gemstones(arr))
+# method 2 using ascii_lowercase
+def gemstones_method_2(arr):
+    from string import ascii_lowercase
+    all_chars = set(ascii_lowercase)
+    for i in arr:
+        all_chars = all_chars.intersection(set(i))
+        print(all_chars)
+    return all_chars
+
+
+print(gemstones_method_2(arr))
