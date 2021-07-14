f82762b76e61b7d6b830eda134fabf53bdd6eb02
Vinaykumar Reddy Kakanuru <kvinaykumarreddy1995@gmail.com>
2021-07-14 21:15:37 +0530

    added time complexity

--- InsertionSort.py
+++ InsertionSort.py
@@ -5,6 +5,12 @@ l = [19,2,31,45,30,11,121,27]
 # Then we pick the third element and find its proper position among the previous two sorted elements.
 # This way we gradually go on adding more elements to the already sorted list by putting them in their proper position.
 
+## Time Complexity
+# Insertion Sort is an easy-to-implement, stable sorting algorithm with 
+# time complexity of O(n²) in the average and worst case, and O(n) in the best case. 
+# For very small n, Insertion Sort is faster than more efficient algorithms such as Quicksort or Merge Sort.
+
+
 def insertion_sort(list):
     for i in range(1, len(list)):
         j = i - 1
