e755e8de1b8701d2f71ac959f6ec355d8c803c61
Vinaykumar Reddy Kakanuru <kvinaykumarreddy1995@gmail.com>
2021-07-14 22:49:58 +0530

    added Time complexity

--- SelectionSort.py
+++ SelectionSort.py
@@ -5,6 +5,15 @@ l = [64, 34, 25, 12, 22, 11, 90]
 # Then we repeat the process for each of the remaining elements in the unsorted list.
 # The next element entering the sorted list is compared with the existing elements and placed at its correct position
 
+## Time Complexity
+# selection sort is an in-place comparison sorting algorithm. 
+# It has an O(n2) time complexity, which makes it inefficient on large lists, 
+# and generally performs worse than the similar insertion sort.
+
+# Worst-case space complexity: O(1) auxiliary
+# Worst-case performance: О(n2) comparisons
+# Best-case performance: О(n2) comparisons
+
 def selection_sort(list):
     for idx in range(len(list)):
         min_idx = idx
