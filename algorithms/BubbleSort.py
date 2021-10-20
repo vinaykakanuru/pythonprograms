725953363560e146851dc4d094d36278820d1ee7
Vinaykumar Reddy Kakanuru <31994314+vinaykakanuru@users.noreply.github.com>
2021-10-20 13:13:59 +0530

    updated bubble sort

--- algorithms/BubbleSort.py
+++ algorithms/BubbleSort.py
@@ -5,6 +5,17 @@ l = [64, 34, 25, 12, 22, 11, 90]
 
 # takes list as input and keeps large number at last pos for each comparison
 
+# Check URL: https://www.guru99.com/bubble-sort.html
+### Time Complexity
+# Worst case – this is where the list provided is in descending order. The algorithm performs the maximum number of executions which is expressed as [Big-O] O(n2)
+# Best case – this occurs when the provided list is already sorted. The algorithm performs the minimum number of executions which is expressed as [Big-Omega] ?(n)
+# Average case – this occurs when the list is in random order. The average Complexity is represented as [Big-theta] ?(n2)
+
+### Space Complexity
+# The space complexity measures the amount of extra space that is needed for sorting the list. 
+# The bubble sort only requires one (1) extra space for the temporal variable used for swapping values. 
+# Therefore, it has a space complexity of O (1).
+
 ### Normal Bubble Sort
 def bubble_sort(list):
     for i in range(len(l)-1, 0, -1):  # keep large number at last
