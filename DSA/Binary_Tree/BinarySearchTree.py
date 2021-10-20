2929980df4f7164b518e15f8e398634b197aaaa1
K VINAY KUMAR REDDY <kvinaykumarreddy1995@gmail.com>
2021-10-21 01:00:31 +0530

    updated BST

--- DSA/Binary_Tree/BinarySearchTree.py
+++ DSA/Binary_Tree/BinarySearchTree.py
@@ -79,11 +79,11 @@ class BinarySearchTree():
 
         # adding Left sub-tree elements to an Array
         if self.left:
-            elements += self.left.pre_order_traversal()
+            elements += self.left.post_order_traversal()
         
         # adding Right sub-tree elements to an Array
         if self.right:
-            elements += self.right.pre_order_traversal()
+            elements += self.right.post_order_traversal()
 
         # adding Root element to an Array
         elements.append(self.data)
@@ -108,6 +108,31 @@ class BinarySearchTree():
         # recursive way to reach the extreme Right node of a given BSTree
         return self.right.find_max()
 
+    def delete_node(self, val):
+        # left sub-tree
+        if val < self.data:
+            if self.left:
+                self.left = self.left.delete_node(val)
+
+        # right sub-tree
+        elif val > self.data:
+            if self.right:
+                self.right = self.right.delete_node(val)
+
+        else:
+            if self.left is None and self.right is None:
+                return None
+            if self.left is None:
+                return self.right
+            if self.right is None:
+                return self.right
+            
+            min_val = self.right.find_min()
+            self.data = min_val
+            self.right = self.right.delete_node(min_val)
+        
+        return self
+
 
 # Helper code to create a BST
 def build_tree(elements):
@@ -120,15 +145,24 @@ def build_tree(elements):
     return root
 
 
-# Driver code
+# Driver code for String values
 countries = ["India","Pakistan","Germany", "USA","China","India","UK","USA"]
 country_tree = build_tree(countries)
-
 print("UK is in the list? ", country_tree.search("UK"))
 print("Sweden is in the list? ", country_tree.search("Sweden"))
+country_tree.delete_node("Pakistan")
+print("Deleting 'Pakistan' from BST in-order traversal:: ",country_tree.in_order_traversal())
+print("Deleting 'Pakistan' from BST pre-order traversal:: ",country_tree.pre_order_traversal())
+print("Deleting 'Pakistan' from BST post-order traversal:: ",country_tree.post_order_traversal())
 
+# Driver code for Int values
 numbers_tree = build_tree([17, 4, 1, 20, 9, 23, 18, 34])
-print("In order traversal gives this sorted list:",numbers_tree.in_order_traversal())
+print("In-order traversal:: ",numbers_tree.in_order_traversal())
+print("Pre-order traversal:: ",numbers_tree.pre_order_traversal())
+print("Post-order traversal:: ",numbers_tree.post_order_traversal())
+numbers_tree.delete_node(17)
+print("After Deleting 17 from BST pre-order traversal:: ",numbers_tree.pre_order_traversal())
+
 
 # Helper code to create a BST
 root = BinarySearchTree(4)
@@ -138,8 +172,12 @@ root.left.left = BinarySearchTree(1)
 root.left.right = BinarySearchTree(3)
 
 # Driver code
-print(root.in_order_traversal())
-print(root.pre_order_traversal())
-print(root.post_order_traversal())
-print(root.find_min())
-print(root.find_max())
+print("In-order Traversal of BST:: ", root.in_order_traversal())
+print("Pre-order Traversal of BST:: ", root.pre_order_traversal())
+print("Post-order Traversal of BST:: ", root.post_order_traversal())
+print("Min of BST:: ", root.find_min())
+print("Max of BST:: ", root.find_max())
+root.delete_node(4)
+print("Delete 4 from BST in-order traversal:: ", root.in_order_traversal())
+print("Delete 4 from BST pre-order traversal:: ", root.pre_order_traversal())
+print("Delete 4 from BST post-order traversal:: ", root.post_order_traversal())
