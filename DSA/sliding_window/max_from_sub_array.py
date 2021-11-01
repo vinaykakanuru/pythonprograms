d7586fa81ff1565ca846da636def30e9c9d4a3b6
Kakanuru, Vinay Kumar Reddy <kvinaykumarreddy1995@gmail.com>
2021-11-02 00:02:47 +0530

    added more test cases with outputs

--- DSA/sliding_window/max_from_sub_array.py
+++ DSA/sliding_window/max_from_sub_array.py
@@ -1,11 +1,19 @@
 ## from given window size of sub_arrays find the maximum number from the provided array
 
-## Input:: arr = [1,3,-1,-3,5,3,6,7] and k = 3
-## Output:: 4
+## Input:: arr = [1, 3, -1, -3, 5, 3, 6, 7] and k = 3
+## Output:: Array of [len(arr) - k + 1] elements
 
-
-arr = [1,3,-1,-3,5,3,6,7]
+## Test case 1
+arr = [1, 3, -1, -3, 5, 3, 6, 7]
 k = 3 
+output = [3, 3, 5, 5, 6, 7]
+
+## Test case 2
+arr = [1, 24, 7, 9, 1, 8, 2, 65, 8, 9, 3, 6]
+# k = 3  ==> output = [24, 24, 9, 9, 8, 65, 65, 65, 9, 9]
+k = 5
+output = [24, 24, 9, 65, 65, 65, 65, 65]
+
 
 def max_from_sub_array(arr, k):
     i, j = 0, 0
